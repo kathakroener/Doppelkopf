8be224009bab83cfbdf812a0a7fb17424f3f761d
Katharina Kröner <katharina_kroener@web.de>
2016-08-12 11:17:41 +0200

    JavaScript-Code für den WebSocket-Chat

---
+++ web/js/chat.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var webSocket;
+var serverURL = "ws://localhost:8080/Doppelkopf/chat";
+
+function sendMessage() {
+    var msg = '{"text":"' + $(inputChatEingabe).val() + '", "sender":"'
+                + $username + '", "received":""}';
+    webSocket.send(msg);
+    $(inputChatEingabe).val('').focus();
+}
+
+function onMessageReceived(evt) {
+    var msg = JSON.parse(evt.data);
+    var $messageLine = $('<tr><td>' + msg.empfangen
+				+ '</td><td>' + msg.sender
+				+ '</td><td>' + msg.text
+				+ '</td></tr>');
+    $('#chatVerlauf').append($messageLine);
+}
+
+$(document).ready(function() {
+    $username = $('#senderUsername').text();
+    
+    webSocket = new WebSocket(serverURL);
+    webSocket.onmessage = onMessageReceived;
+    
+    $('#buttonSendMessage').click(function(evt) {
+			sendMessage();
+		}); 
+    
+})
